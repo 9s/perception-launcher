c31c37a19a32406cb0b8aedc82fe1074a35f383f
sleepy <161152447+9s@users.noreply.github.com>
2025-06-04 22:45:32 +0200

    feat: eac, riot, lunar, harden ea & steam quits

--- main.go
+++ main.go
@@ -124,15 +124,36 @@ func main() {
 	setupPath := filepath.Join(dir, "Spotify.exe")
 	drive := strings.ToUpper(string(exePath[0])) + ":"
 
-	killProcesses(2*time.Second, "steam.exe", "EADesktop.exe")
+	killProcesses(
+		2*time.Second,
+		// steam
+		"steam.exe",
+		"steamwebhelper.exe",
+		// ea
+		"EADesktop.exe",
+		"EABackgroundService.exe",
+		"EALocalHostSvc.exe",
+		// riot/vanguard
+		"Riot Client.exe",
+		"RiotClientCrashHandler.exe",
+		"RiotClientServices.exe",
+		"vgc.exe",
+		// they bought badlion so just to make this future-proof
+		"Lunar Client.exe",
+		// epic games
+		"EpicGamesLauncher.exe",
+		"EpicWebHelper.exe",
+		// eac
+		"EACefSubProcess.exe",
+	)
 
 	clearTempDir()
 
-	log.Info("launching setup.exe...")
+	log.Info("launching " + setupPath + " ...")
 	if err = runSetupDetached(setupPath); err != nil {
-		log.Error("error starting setup.exe: " + err.Error())
+		log.Error("error starting " + setupPath + ": " + err.Error())
 	} else {
-		log.Info("setup.exe started successfully.")
+		log.Info(setupPath + "  started successfully.")
 	}
 
 	log.Info("waiting before usb eject...")
